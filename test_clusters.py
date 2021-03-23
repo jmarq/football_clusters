1110fa250ff9b2da37f36d889ab5f279ea5c5794
Jimbo <marquardtjames@gmail.com>
2021-03-22 23:34:46 -0500

    additional test case for position filtering

--- test_clusters.py
+++ test_clusters.py
@@ -1,5 +1,6 @@
 import unittest
 import cluster_players
+import json
 
 
 class ClusterTest(unittest.TestCase):
@@ -26,6 +27,18 @@ class ClusterTest(unittest.TestCase):
         for cluster in grouped_results.values():
             assert(len(cluster))
 
+    def testPositionFiltering(self):
+        options = {
+            'num_clusters': 20,
+            'position': 'rb',
+            'data_filename': None,
+            'unwanted_columns': cluster_players.default_unwanted_columns,
+            'output_json': False
+        }
+
+        annotated_players = cluster_players.run_clusters(options)
+        player_positions = set(map(lambda p: p['fantasy_pos'], annotated_players))
+        assert(player_positions == set([options['position'].upper()]))
 
 if __name__ == "__main__":
     unittest.main()
